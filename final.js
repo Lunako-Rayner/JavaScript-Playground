d0195a4542cc2267a40441b5602dc78ef5e77cf7
Lunako Rayner <lunao.rayner@gmail.com>
2023-06-16 18:08:53 +0100

    added balls

--- final.js
+++ final.js
@@ -2,48 +2,91 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+const cellW = canvas.width;
+const cellH = Math.floor(canvas.height / 5);
+const ballSpeedMax = 1;
+const ballCount = 100;
 
+//handle resize of window
 window.addEventListener('resize', function () {
 
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    ctx.beginPath();
-    ctx.arc(100, 75, 50, 0, Math.PI * 2);
-    ctx.strokeStyle = 'red';
-    ctx.stroke();
+    // ctx.beginPath();
+    // ctx.arc(100, 75, 50, 0, Math.PI * 2);
+    // ctx.strokeStyle = 'red';
+    // ctx.stroke();
 });
 
+const randomFloat = (left, right) => {
+    return Math.random() * (right - left) + left;
+};
+
 class Ball {
-    constructor() {
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
-        this.size = Math.random() * 50;
-        this.sizeChangeSpeed = 2;
-        this.speedX = Math.random() * 10 - 5;
-        this.speedY = Math.random() * 10 - 5;
-        this.colour = 'red';
+    constructor(ballSpeedMax) {
+        this.radius = randomFloat(0, 50);
+        this.r = randomFloat(0, 255);
+        this.g = randomFloat(0, 255);
+        this.b = randomFloat(0, 255);
+        this.xSpeed = randomFloat(-ballSpeedMax, ballSpeedMax);
+        this.ySpeed = randomFloat(-ballSpeedMax, ballSpeedMax);
+
+        this.x = randomFloat(this.radius, cellW - this.radius);
+        this.y = randomFloat(this.radius, cellH - this.radius);
     }
     update() {
-        this.x += this.speedX;
-        this.y += this.speedY;
-        this.size += this.sizeChangeSpeed;
-        this.size = Math.abs(this.size)
-        this.sizeChangeSpeed = Math.random() * 4 - 2
+        this.x += this.xSpeed;
+        this.y += this.ySpeed;
+
+        if (this.x <= 0 + this.radius || this.x >= cellW - this.radius) {
+            this.xSpeed *= -1;
+            this.r = randomFloat(0, 255);
+            this.b = randomFloat(0, 255);
+            this.g = randomFloat(0, 255);
+        };
+        if (this.y <= 0 + this.radius || this.y >= cellH - this.radius) {
+            this.ySpeed *= -1;
+            this.r = randomFloat(0, 255);
+            this.b = randomFloat(0, 255);
+            this.g = randomFloat(0, 255);
+        };
     }
     draw() {
         ctx.beginPath();
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-        ctx.strokeStyle = this.colour;
-        ctx.stroke();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fillStyle = `rgba(${this.r},${this.g}, ${this.b})`;
+        ctx.fill();
     }
 }
 
-var ball = new Ball();
 
+class Balls {
+    constructor(ballCount, ballSpeedMax) {
+        this.ballsArr = [];
+
+        for (let i = 0; i < ballCount; i++) {
+            this.ballsArr.push(new Ball(ballSpeedMax));
+        }
+    }
+    updateAllBalls() {
+        for (var ball of this.ballsArr) {
+            ball.update();
+        }
+    }
+    drawAllBalls() {
+        for (var ball of this.ballsArr) {
+            ball.draw();
+        }
+
+    }
+}
+
+var balls = new Balls(ballCount, ballSpeedMax);
 const animate = function () {
-    ctx.clearRect(0,0, canvas.width, canvas.height)
-    ball.update();
-    ball.draw();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    balls.updateAllBalls();
+    balls.drawAllBalls();
     requestAnimationFrame(animate);
 };
 animate();
+
